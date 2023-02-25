73aafc566b17f96547b68e2f52ad9de5f9e9400b
Arugaf <arug113@gmail.com>
2023-02-26 02:50:55 +0300

    fix sprint coroutine logic and add player to main scene

--- Assets/Player/Sprinter.cs
+++ Assets/Player/Sprinter.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 namespace Player {
     [RequireComponent(typeof(Stamina))]
+    [RequireComponent(typeof(PlayerMovement))]
     public class Sprinter : MonoBehaviour {
         [SerializeField] private uint sprintCost = 10;
         [SerializeField] private float staminaSubtractionDelay = 0.5f;
@@ -11,44 +12,44 @@ namespace Player {
         private Stamina _stamina;
         private PlayerMovement _playerMovement;
 
+        private Coroutine _sprintCoroutine;
+        private IEnumerator _sprintEnumerator;
+
         private bool _sprinting;
 
         private void Start() {
+            _sprintEnumerator = Sprint();
             _stamina = GetComponent<Stamina>();
             _playerMovement = GetComponent<PlayerMovement>();
         }
 
-        private void Update() {
-            if (_sprinting) {
-                StartCoroutine(Sprint());
-            }
-        }
-
         public void StartSprint() {
+            if (_sprintCoroutine != null || _stamina.GetCurrentStamina() <= 0f) return;
+
             _sprinting = true;
             _playerMovement.IncreaseSpeed(speedBoost);
+            _sprintCoroutine = StartCoroutine(_sprintEnumerator);
         }
 
         public void EndSprint() {
+            if (_sprintCoroutine == null) return;
+
             _sprinting = false;
             _playerMovement.DecreaseSpeed(speedBoost);
+            StopCoroutine(_sprintCoroutine);
+            _sprintCoroutine = null;
         }
 
         private IEnumerator Sprint() {
             while (_sprinting) {
-                if (!_playerMovement.IsMoving) {
-                    yield return null;
+                if (_playerMovement.IsMoving) {
+                    if (!_stamina.RemoveStamina(sprintCost)) {
+                        EndSprint();
+                    }
                 }
 
-                if (_stamina.RemoveStamina(sprintCost)) {
-                    yield return new WaitForSeconds(staminaSubtractionDelay);
-                }
-
-                EndSprint();
-                yield break;
+                yield return new WaitForSeconds(staminaSubtractionDelay);
             }
-
-            EndSprint();
         }
     }
 }
